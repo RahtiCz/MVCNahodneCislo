7e6e188e2924a5449b3a7e77107544df9e40a8dd
Marek Janouch <janouch.marek@centrum.cz>
2020-11-11 17:23:56 +0100

    Add project files.

---
+++ MVCNahodneCislo/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCNahodneCislo.Models;
+
+namespace MVCNahodneCislo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Generator generator = new Generator();
+            ViewBag.Cislo = generator.VratCislo();
+            return View();
+        }
+    }
+}
